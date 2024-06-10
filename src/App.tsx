a4e18ea3229424052a0ec56da6f03afa28109d8a
Jinsu Choi <jschoi9160@naver.com>
2024-06-10 11:22:00 +0900

    Feat : Add new routes for various pages
    - MainPAge
    - LoginPage
    - JoinPage
    - IDEPage
    - MyPage
    - QuestPage
    - RankPage
    - BattlePage
    - ChangePwPage
    - BoardPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import MainPage from './pages/MainPage/MainPage';
+import LoginPage from './pages/LoginPage/LoginPage';
+import JoinPage from './pages/JoinPage/JoinPage';
+import IDEPage from './pages/IDEPage/IDEPage';
+import MyPage from './pages/MyPage/MyPage';
+import QuestListPage from './pages/QuestListPage/QuestListPage';
+import RankPage from './pages/RankPage/RankPage';
+import BattlePage from './pages/BattlePage/BattlePage';
+import ChangePwPage from './pages/ChangePwPage/ChangePwPage';
+import BoardPage from './pages/BoardPage/BoardPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/join" element={<JoinPage />} />
+      <Route path="/ide" element={<IDEPage />} />
+      <Route path="/my" element={<MyPage />} />
+      <Route path="/quest" element={<QuestListPage />} />
+      <Route path="/rank" element={<RankPage />} />
+      <Route path="/battle" element={<BattlePage />} />
+      <Route path="/changepw" element={<ChangePwPage />} />
+      <Route path="/board" element={<BoardPage />} />
+    </Routes>
   );
 }
 
