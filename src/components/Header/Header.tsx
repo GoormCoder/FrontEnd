55a9920a3e596acb086ac8c8d516bc8cf60083cf
yoglee64 <l3060921@naver.com>
2024-06-10 23:39:24 +0900

    add Header draft

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,115 @@
-import React from 'react'
+import React, { useState } from 'react'
+import main_logo from '../../assets/goorm_coder_logo.png'
+import styled, { keyframes } from 'styled-components'
+import hamburgerButton from '../../assets/hamburger.png'
+
+const HeaderContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-bottom: 1px solid
+
+`;
+const SidebarMenu = styled.div<{ isOpen: boolean }>`
+    position: fixed;
+    height: 100vh;
+    width: 250px;
+    top: 0;
+    right: 0;
+    display: flex;
+    padding: 20px
+    flex-direction: column;
+    background-color: rgba(0, 0, 0, 0.5);
+    transition: background-color 0.3s;
+    transform: translateX(${props => (props.isOpen ? '90%' : '100%')});
+    animation: ${props => (props.isOpen ? slideIn : slideOut)} 0.3s forwards;
+`;
+const slideIn = keyframes`
+    from {
+        transform: translateX(100%);
+        background-color: transparent;
+    }
+    to {
+        transform: translateX(90%);
+        background-color: rgba(0, 0, 0, 0.5);
+    }
+`;
+
+const slideOut = keyframes`
+    from {
+        transform: translateX(90%);
+        background-color: rgba(0, 0, 0, 0.5);
+    }
+    to {
+        transform: translateX(100%);
+        background-color: transparent;
+    }
+`;
+const LeftSection = styled.div`
+    display: flex;
+    align-items: center;
+    margin-left: 100px;
+`;
+
+const RightSection = styled.div`
+    display: flex;
+    align-items: center;
+`;
+const Logo = styled.img`
+    width: 73px;
+`;
+const LogoText = styled.p`
+    margin: 0;
+`;
+const LoginText = styled.p`
+    display: flex;    
+    margin: 0;  
+    margin-right: 10px;
+`;
+const SideBar = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-left: auto;
+    width: 73px; 
+    border: 1px solid;
+    border-radius: 5px;
+    height: 50px;
+    width: 50px;
+    margin-right: 100px;
+    background-color: rgba(217,217,217,0.5);
+    cursor: pointer;
+`;
+
 
 const Header = () => {
+    const [isSidebarOpen, setisSidebarOpen] = useState(false);
+
+    const toggleSidebar = () => {
+        setisSidebarOpen(!isSidebarOpen);
+    };
+
     return (
-        <div>
-            Header
-        </div>
+        <>
+            <HeaderContainer>
+                <LeftSection>
+                    <Logo src={main_logo} alt='main logo' />
+                    <LogoText>Goorm Coder</LogoText>
+                </LeftSection>
+                <RightSection>
+                    <LoginText>로그인</LoginText>
+                    <SideBar onClick={toggleSidebar}>
+                        <img src={hamburgerButton} alt='hamburger button' width="30px" />
+                    </SideBar>
+                </RightSection>
+            </HeaderContainer>
+            <SidebarMenu isOpen={isSidebarOpen}>
+                <h2>Menu</h2>
+                <p>Menu Item 1</p>
+                <p>Menu Item 2</p>
+                <p>Menu Item 3</p>
+            </SidebarMenu>      
+        </>
     )
 }
 
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Header from '../../components/Header/Header'
 
 const MainPage = () => {
     return (
         <div>
+            <Header />
             MainPage
         </div>
     )
