44e2f67d6101184eb82f8ac99553ad0ee8108ba9
Jinsu Choi <jschoi9160@naver.com>
2024-06-11 20:15:39 +0900

    Feat : Add Solve State Search feature
    문제 풀이 상태 검색 기능 추가
    - 안 푼 문제 (DB 정답 여부 : null)
    - 풀고 있는 문제 (DB 정답 여부 : 오답)
    - 푼 문제 (DB 정답 여부 : 정답)

--- src/pages/QuestListPage/QuestListPage.tsx
+++ src/pages/QuestListPage/QuestListPage.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef, useState } from 'react'
 import styled from 'styled-components'
 
 type Quest = {
+    state: string | null,
     title: string,
     level: string,
     correctPeople: number,
@@ -11,22 +12,23 @@ type Quest = {
 type Tag = string
 
 interface SelectProps {
+    stateDisplay: string,
     levelDisplay: string,
     languageDisplay: string
 }
 
 // 임시 문제 리스트
 const questList: Quest[] = [
-    { title: '같은 숫자는 싫어', level: 'Level 1', correctPeople: 10, accuracy: '90%' },
-    { title: '기능개발', level: 'Level 2', correctPeople: 48, accuracy: '80%' },
-    { title: '올바른 괄호', level: 'Level 2', correctPeople: 35, accuracy: '50%' },
-    { title: '프로세스', level: 'Level 2', correctPeople: 15, accuracy: '50%' },
-    { title: '다리를 지나는 트럭', level: 'Level 2', correctPeople: 77, accuracy: '80%' },
-    { title: '주식가격', level: 'Level 2', correctPeople: 5, accuracy: '80%' },
-    { title: '체육복', level: 'Level 1', correctPeople: 50, accuracy: '60%' },
-    { title: '섬 연결하기', level: 'Level 3', correctPeople: 52, accuracy: '50%' },
-    { title: '단속카메라', level: 'Level 3', correctPeople: 95, accuracy: '90%' },
-    { title: '큰 수 만들기', level: 'Level 2', correctPeople: 35, accuracy: '50%' },
+    { state: "T", title: '같은 숫자는 싫어', level: 'Level 1', correctPeople: 10, accuracy: '90%' },
+    { state: "T", title: '기능개발', level: 'Level 2', correctPeople: 48, accuracy: '80%' },
+    { state: "F", title: '올바른 괄호', level: 'Level 2', correctPeople: 35, accuracy: '50%' },
+    { state: null, title: '프로세스', level: 'Level 2', correctPeople: 15, accuracy: '50%' },
+    { state: "F", title: '다리를 지나는 트럭', level: 'Level 2', correctPeople: 77, accuracy: '80%' },
+    { state: "F", title: '주식가격', level: 'Level 2', correctPeople: 5, accuracy: '80%' },
+    { state: null, title: '체육복', level: 'Level 1', correctPeople: 50, accuracy: '60%' },
+    { state: "T", title: '섬 연결하기', level: 'Level 3', correctPeople: 52, accuracy: '50%' },
+    { state: "F", title: '단속카메라', level: 'Level 3', correctPeople: 95, accuracy: '90%' },
+    { state: null, title: '큰 수 만들기', level: 'Level 2', correctPeople: 35, accuracy: '50%' },
 ];
 
 // 임시 유저 이름
@@ -34,12 +36,15 @@ const userName: string = "최진수(닉네임)";
 
 
 const QuestListPage = () => {
+    const stateSelectRef = useRef<HTMLDivElement | null>(null);
     const levelSelectRef = useRef<HTMLDivElement | null>(null);
     const languageSelectRef = useRef<HTMLDivElement | null>(null);
 
     const [searchText, setSearchText] = useState<string>("");
     const [tagList, setTagList] = useState<Tag[]>([]);
     const [levelList, setLevelList] = useState<string[]>([]);
+    const [stateList, setStateList] = useState<(string | null)[]>([]);
+    const [stateDisplay, setStateDisplay] = useState<string>('none');
     const [levelDisplay, setLevelDisplay] = useState<string>('none');
     const [languageDisplay, setLanguageDisplay] = useState<string>('none');
     const [searchResult, setSearchResult] = useState<Quest[]>(questList);
@@ -49,12 +54,16 @@ const QuestListPage = () => {
 
     useEffect(() => {
         function handleClickOutSide(e: MouseEvent) {
+            if (stateSelectRef.current && !stateSelectRef.current.contains(e.target as Node)) {
+                setStateDisplay('none');
+            }
             if (levelSelectRef.current && !levelSelectRef.current.contains(e.target as Node)) {
                 setLevelDisplay('none');
             }
             if (languageSelectRef.current && !languageSelectRef.current.contains(e.target as Node)) {
                 setLanguageDisplay('none');
             }
+
         }
 
         document.addEventListener('mouseup', handleClickOutSide);
@@ -71,6 +80,7 @@ const QuestListPage = () => {
         if (checked) {
             setTagList(pre => [...pre, tagValue]);
             if (type == "level") setLevelList(pre => [...pre, tagValue]);
+            if (type == "state") setStateList(pre => [...pre, tagValue]);
         } else {
             removeTag(tagValue);
         }
@@ -82,15 +92,23 @@ const QuestListPage = () => {
 
         setTagList(pre => pre.filter(tag => tag !== tagValue));
         setLevelList(pre => pre.filter(tag => tag !== tagValue));
+        setStateList(pre => pre.filter(tag => tag !== tagValue));
     }
 
     function search(e: any, searchText: string, questList: Quest[]): void {
         if (e) e.preventDefault();
+
         const searchResult: Quest[] = questList.filter(quest => {
+            let state: string = "";
+            if (quest.state == "T") state = "푼 문제";
+            if (quest.state == "F") state = "풀고 있는 문제";
+            if (quest.state == null) state = "안 푼 문제";
             const nonBlankSearchText = removeBlank(searchText);
             const nonBlankQuestTitle = removeBlank(quest.title);
-            if (!levelList.length) return nonBlankQuestTitle.includes(nonBlankSearchText)
-            return nonBlankQuestTitle.includes(nonBlankSearchText) && levelList.includes(quest.level);
+            if (!levelList.length && !stateList.length) return nonBlankQuestTitle.includes(nonBlankSearchText)
+            if (!levelList.length && stateList.length) return nonBlankQuestTitle.includes(nonBlankSearchText) && stateList.includes(state);
+            if (levelList.length && !stateList.length) return nonBlankQuestTitle.includes(nonBlankSearchText) && levelList.includes(quest.level);
+            return nonBlankQuestTitle.includes(nonBlankSearchText) && levelList.includes(quest.level) && stateList.includes(state);
         });
         setSearchResult(searchResult);
     }
@@ -111,7 +129,7 @@ const QuestListPage = () => {
     return (
         <QuestListContainer >
             <SearchAndListContainer>
-                <SearchContainer levelDisplay={levelDisplay} languageDisplay={languageDisplay}>
+                <SearchContainer stateDisplay={stateDisplay} levelDisplay={levelDisplay} languageDisplay={languageDisplay}>
                     <form onSubmit={(e) => { search(e, searchText, questList) }}>
                         <input type="search" onChange={(e) => { setSearchText(e.target.value) }} />
                         <input type="submit" value="검색" />
@@ -121,6 +139,31 @@ const QuestListPage = () => {
                     </div>
 
                     <div className="search-select">
+
+                        <div className='select-container' ref={stateSelectRef}>
+                            <div className='select-title'
+                                onClick={() => {
+                                    stateDisplay == 'none' ?
+                                        setStateDisplay('block') : setStateDisplay('none')
+                                }}>
+                                상태 선택 박스
+                            </div>
+                            <div className='select-lists state'>
+                                <div>
+                                    <input type="checkbox" name="안 푼 문제" id="안 푼 문제" onChange={(e) => (setTag("state", e.target.checked, e.target.id))} />
+                                    <label htmlFor="안 푼 문제">안 푼 문제</label>
+                                </div>
+                                <div>
+                                    <input type="checkbox" name="풀고 있는 문제" id="풀고 있는 문제" onChange={(e) => (setTag("state", e.target.checked, e.target.id))} />
+                                    <label htmlFor="풀고 있는 문제">풀고 있는 문제</label>
+                                </div>
+                                <div>
+                                    <input type="checkbox" name="푼 문제" id="푼 문제" onChange={(e) => (setTag("state", e.target.checked, e.target.id))} />
+                                    <label htmlFor="푼 문제">푼 문제</label>
+                                </div>
+                            </div>
+                        </div>
+
                         <div className='select-container' ref={levelSelectRef}>
                             <div className='select-title'
                                 onClick={() => {
@@ -144,6 +187,7 @@ const QuestListPage = () => {
                                 </div>
                             </div>
                         </div>
+
                         <div className='select-container' ref={languageSelectRef}>
                             <div className='select-title'
                                 onClick={() => {
@@ -167,6 +211,7 @@ const QuestListPage = () => {
                                 </div>
                             </div>
                         </div>
+
                     </div>
 
                     <div className="search-tags">
@@ -182,6 +227,7 @@ const QuestListPage = () => {
                     <table>
                         <thead>
                             <tr>
+                                <th>상태</th>
                                 <th>제목</th>
                                 <th>난이도</th>
                                 <th>맞힌 사람</th>
@@ -191,6 +237,13 @@ const QuestListPage = () => {
                         <tbody>
                             {searchResult.map((row, index) => (
                                 <tr key={index}>
+                                    <td>
+                                        {row.state ?
+                                            <>
+                                                {row.state == "T" ? "V" : "-"}
+                                            </>
+                                            : ''}
+                                    </td>
                                     <td>{row.title}</td>
                                     <td>{row.level}</td>
                                     <td>{row.correctPeople}</td>
@@ -263,6 +316,10 @@ const SearchContainer = styled.div<SelectProps>`
         }
     }
 
+    & .state {
+        display: ${props => props.stateDisplay};
+    }
+
     & .level {
         display: ${props => props.levelDisplay};
     }
@@ -280,7 +337,7 @@ const SearchContainer = styled.div<SelectProps>`
         text-align: center;
         border: 1px solid black;
         border-radius: 10px;
-        width: 70px;
+        width: fit-content;
         padding: 5px;
         cursor: default;
     }
